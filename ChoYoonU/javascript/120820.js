2b3d7c7922c38a730ec297af50b2795a84212028
yoonucho <tomorrowcho@gmail.com>
2024-12-09 02:47:36 +0900

    프로그래머스 #120820.나이 출력

---
+++ ChoYoonU/javascript/120820.js
@@ -0,0 +1,10 @@
+/* 프로그래머스 120820.나이 출력
+(https://school.programmers.co.kr/learn/courses/30/lessons/120820) */
+
+function solution(age) {
+	var answer = 0;
+	const year = 2022;
+	answer = year - age + 1;
+	return answer;
+}
+
