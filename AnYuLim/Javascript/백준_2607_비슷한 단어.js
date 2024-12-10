58d5cd5d396bb3051b8920ff37e8238f27a570b8
anyl92 <yulim9592@gmail.com>
2024-12-11 01:03:12 +0900

    test commit

---
+++ AnYuLim/Javascript/백준_2607_비슷한 단어.js
@@ -0,0 +1,15 @@
+// const input = require("fs").readFileSync("../input.txt").toString().trim().split("\n");
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const input = `
+4
+DOG
+GOD
+GOOD
+DOLL
+`
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(input);
