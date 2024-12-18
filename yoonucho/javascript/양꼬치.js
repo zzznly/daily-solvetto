8d904d878425f184c9463cb47e72557ba94dd1ed
yoonucho <tomorrowcho@gmail.com>
2024-12-18 15:38:37 +0900

    Delete yoonucho/javascript directory
    알고풀이로 작성하는 과정에서 초반에 영문 이름을 기존과 다른 이름으로 작성해 불필요한 디렉토리가 생성되었습니다. 이를 삭제하고 싶습니다.

--- yoonucho/javascript/양꼬치.js
+++
@@ -1,9 +0,0 @@
-function solution(n, k) {
-    var answer = 0;
-    if(n < 10){
-        answer = n * 12000 + k * 2000
-    } else {
-        answer = n * 12000 + k * 2000 - Math.trunc(n/10) * 2000;
-    }
-    return answer;
-}
