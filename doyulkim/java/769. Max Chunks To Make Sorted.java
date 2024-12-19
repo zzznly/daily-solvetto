092e38aaec8495a80a757d021ecbd2dcec3bd4d2
Rim Wongi <85725033+Voyager003@users.noreply.github.com>
2024-12-19 20:10:38 +0900

    Merge: [java] 769. Max Chunks To Make Sorted
    Auto-merged by Slack bot

---
+++ doyulkim/java/769. Max Chunks To Make Sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int max = 0;
+        int chunk = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                chunk++;
+            }
+        }
+
+        return chunk;
+    }
+}
