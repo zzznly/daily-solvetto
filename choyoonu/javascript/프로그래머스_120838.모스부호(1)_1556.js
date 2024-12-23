a75603eee086df17c1f02c1a639fe46cea899154
Rim Wongi <85725033+Voyager003@users.noreply.github.com>
2024-12-23 15:56:30 +0900

    Merge: [javascript] 프로그래머스_120838.모스부호(1)
    Auto-merged by Slack bot

---
+++ choyoonu/javascript/프로그래머스_120838.모스부호(1)_1556.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    var answer = '';
+    const splitLetter= letter.split(' ');
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    answer = splitLetter.map((v)=>morse[v]).join('');
+    
+    return answer;
+}
